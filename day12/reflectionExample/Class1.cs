58b726ba70259f1515dd629b592785a3d6934b64
shendeshraddha1996 <65154450+shendeshraddha1996@users.noreply.github.com>
2020-12-20 21:55:32 +0530

    Add files via upload

---
+++ day12/reflectionExample/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianCal
+{
+    class Class1
+    {
+        public decimal Basic { get; set; }
+        public decimal NetSalary()
+        {
+            return Basic * 2M;
+        }
+    }
+}
---
+++ day12/reflectionExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace reflectionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly asm = Assembly.LoadFile(@"C:\Users\shradhha shende\source\repos\day12_1\reflectionExample\Class1.cs");
+            Type t = asm.GetType(asm.GetName().Name + ".TaxCalculator", true, true);
+            object obj = Activator.CreateInstance(t);
+            PropertyInfo p = t.GetProperty("Basic");
+            p.SetValue(obj,10000M,null);
+            Console.WriteLine(p.GetValue(obj,null).ToString());
+            MethodInfo m = t.GetMethod("GetNetSalary");
+            decimal NetSal = (decimal)m.Invoke(obj, null);
+            Console.WriteLine(NetSal.ToString());
+        }
+    }
+}
