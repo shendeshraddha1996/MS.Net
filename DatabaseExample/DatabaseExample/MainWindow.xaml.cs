7510d80f4b63d37d7e265f2440c6707aa327f3fb
shendeshraddha1996 <65154450+shendeshraddha1996@users.noreply.github.com>
2020-12-20 21:58:10 +0530

    Add files via upload

---
+++ DatabaseExample/DatabaseExample/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DatabaseExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MsSqlLocalDb;Initial Catalog=JKDec20;Integrated Security=true";
+           // cn.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB; AttachDbFilename = "C:\Users\shradhha shende\source\repos\MyDatabaseDemo.mdf"; Integrated Security = True; Connect Timeout = 30";
+            cn.Open();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cn;
+            cmd.CommandType = CommandType.Text;
+
+            cmd.CommandText = "insert into UserLogin values(@Name,@Password)";
+
+            cmd.Parameters.AddWithValue("@Name", txtName.Text);
+            cmd.Parameters.AddWithValue("@Password", txtPassword.Text);
+        
+            //MessageBox.Show(cmd.CommandText);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("okay");
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            cn.Close();
+            MessageBox.Show("Successfully closed");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            SqlConnection cn = new SqlConnection();
+             cn.ConnectionString = @"Data Source=(localdb)\MsSqlLocalDb;Initial Catalog=JKDec20;Integrated Security=true";
+            //cn.ConnectionString=@"(LocalDB)\MSSQLLocalDB; AttachDbFilename = "C:\Users\shradhha shende\source\repos\MyDatabaseDemo.mdf"; Integrated Security = True; Connect Timeout = 30";
+            cn.Open();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cn;
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.CommandText = "InsertUser";
+
+            cmd.Parameters.AddWithValue("@Name", txtName.Text);
+            cmd.Parameters.AddWithValue("@Password", txtPassword.Text);
+
+            //MessageBox.Show(cmd.CommandText);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("okay");
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            cn.Close();
+            MessageBox.Show("Successfully closed");
+
+        }
+    }
+}
